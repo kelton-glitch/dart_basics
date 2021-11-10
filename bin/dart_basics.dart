a89fb6f71cafd3cf34da1ebd12e33435c206b35d
Kamgo N Elton <76106656+kelton-glitch@users.noreply.github.com>
2021-11-10 23:33:06 +0100

    Initial commit

---
+++ bin/dart_basics.dart
@@ -0,0 +1,76 @@
+import 'dart:math';
+
+void main() {
+  //print(2 + 8);
+  //print(22 ~/ 7);  /* The sign ~ is used in fraction which could remainders we do not need.*/
+
+  //print((8000 / (5 * 10) - 32) ~/ (29 % 5));
+
+  /*print(350 / 5 + 2);
+  print(350 / (5 + 2));*/
+
+  // print(sin(45 * pi / 180));
+
+//  print(cos(135 * pi / 180));
+
+  //print(sqrt(2));
+
+  //var someNumber = 10;
+  //someNumber = 17;
+
+  //int myAge = 21;
+  //print(myAge);
+
+  //double averageAge = 21;
+  //print((myAge+ 20)/2);
+
+// Mini-exercises
+  /* const testNumber = 20;
+  const evenOdd = testNumber % 2;
+  print(evenOdd);*/
+
+  ///CHALLENGES
+  ///CHALLENGE 1
+  /*const myAge = 21;
+  int dogs = 1;
+  dogs += 1;
+  print(dogs);*/
+
+  ///CHALLENGE 2
+  /*int age = 16;
+  print(age);
+  age = 30;
+  print(age);*/
+
+  ///CHALLENGE 3
+  /*const x = 46;
+  const y = 10;
+  const answer1= (x * 100) + y;
+  const answer2= (x * 100) + (y * 100);
+  const answer3= (x * 100) + (y / 10);
+   
+
+  print(answer1);
+  print(answer2);
+  print(answer3);*/
+
+  ///CHALLENGE 4
+  /*const double rating1 = 44;
+  const double rating2 = 29;
+  const double rating3 = 91;
+
+  const double averageRating = ((rating3 + rating2 + rating1) / 3);
+
+  print(averageRating);*/
+
+  ///CHALLENGE 5
+  const double a = 3;
+  const double b = 7;
+  const double c = 2;
+
+  double root1 = (-(b) + sqrt(b * b - 4 * a * c)) / (2 * a);
+ double root2 = (-(b) - sqrt(b * b - 4 * a * c)) / (2 * a);
+
+  print(root1);
+  print(root2);
+}
